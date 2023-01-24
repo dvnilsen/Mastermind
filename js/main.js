d4df37c2f9b56820d309001f029b3abb32ea5f6e
dvnilsen <dvnilsen@protonmail.com>
2023-01-23 16:29:22 -0800

    added constants and variables to JS

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 /*----- constants -----*/
-
+const colorsArray = []; 
 
 /*----- state variables -----*/
-
+let board; // array of arrays
+let winner; // null = no winner; 1 or - 1 = winner
 
 /*----- cashed elements -----*/ 
 
 
 /*----- event listeners -----*/
-
+//create one event listener for all buttons, connect buttons to colorsArray
 
 /*----- functions -----*/ 
 

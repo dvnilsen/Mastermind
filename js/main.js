238012acd764c7cafec5bf241db613d1c1e687f3
dvnilsen <dvnilsen@protonmail.com>
2023-01-24 10:57:58 -0800

    css styling to show board and computer code row correctly

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ const colorsArray = [];
 
 /*----- state variables -----*/
 let board; // array of arrays
+// need separate array for the computer's response 
+
 let winner; // null = no winner; 1 or - 1 = winner
+let turn;
+
+// arrays to store guesses and responses for each turn
+let playerGuess = [];
+let computerResponse = []; 
 
 /*----- cashed elements -----*/ 
 
@@ -12,4 +19,9 @@ let winner; // null = no winner; 1 or - 1 = winner
 //create one event listener for all buttons, connect buttons to colorsArray
 
 /*----- functions -----*/ 
+
+init();
+render(); 
+secretCode(); 
+checkWin();
 

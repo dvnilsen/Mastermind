f47f919cdd7becbf0982860572d63ac2123d9fcd
dvnilsen <dvnilsen@protonmail.com>
2023-01-24 16:46:49 -0800

    added class to divs, added colors array, added functions to add colors to board

--- js/main.js
+++ js/main.js
@@ -1,52 +1,58 @@
 /*----- constants -----*/
-const colorArray = []; 
-const colorButtons = []; 
+const COLORS = ["blue", "green", "yellow", "red", "white", "black"];
 
 /*----- state variables -----*/
 let board; // array of arrays
 // need separate array for the computer's response 
-let choices; // array for the player's guesses
 let code; // computer's secret code 
 let winner; // null = no winner; 1 or - 1 = winner
 let turn;
 let computerResponse; // array of matches and misses in the player's guess (the "choices" array)
+let currentColor;
 
 /*----- cashed elements -----*/ 
-
+const choiceBtns = document.getElementById("choices");
 
 /*----- event listeners -----*/
-//create one event listener for all buttons, connect buttons to colorsArray
+    // need listeners on each color button to fill board array
+choiceBtns.addEventListener("click", choiceHandler);
 
 /*----- functions -----*/ 
 init(); 
 
 function init() {
-    board = [
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null],
-        [null, null, null, null, null, null, null, null, null, null],
-    ];
+    board = [null, null, null, null];
 
-    code = [null, null, null, null];
-
-    choices =  [null, null, null, null];
-
-    computerResponse = [null, null, null, null];
+    code = secretCode();
+    console.log(code); 
+    computerResponse = [];
 
     winner = null;
     turn = 0;
+    currentColor = "red";
 
     render(); 
 };
 
 function render() {
-    renderBoard();
-    renderMessage();
-    renderControls();  
+    //renderBoard();
+    //renderMessage();
+    //renderControls();  
 }; 
 
-//secretCode(); 
+function secretCode() {
+    let tempArray = [];
+    while(tempArray.length !== 4) {
+        tempArray.push(COLORS[Math.floor(Math.random()*COLORS.length)])
+    }
+    return tempArray;
+}; 
+
+function choiceHandler(evt) {
+    if(evt.target.tagName !== "DIV") return;
+    board[parseInt(evt.target.id)] = currentColor;
+    render();
+}
+
 //checkWin();
 
